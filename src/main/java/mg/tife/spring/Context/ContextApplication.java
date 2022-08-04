e3df5ebafb55dbb8a46fa9336290b986e96ca784
eugene <eralaivao@ingenosya.mg>
2022-08-04 16:33:28 +0300

    first commit

---
+++ src/main/java/mg/tife/spring/Context/ContextApplication.java
@@ -0,0 +1,13 @@
+package mg.tife.spring.Context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/tife/spring/Context/ContextApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.tife.spring.Context;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
